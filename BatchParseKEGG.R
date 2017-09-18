9be09ab7b53a0966ba8a2958d3cce0ec116c2c45
mycecilia <shiyuchenwisc@gmail.com>
2017-09-18 12:50:01 -0500

    Update and rename Batch Parse KEGG to BatchParseKEGG.R

--- BatchParseKEGG.R
+++ BatchParseKEGG.R
@@ -1,11 +1,13 @@
-##' InputFiles is a list of .keg file names for parsing
-##' Read the parsed files into R in batches
+##' Integrate KEGG database into analysis pipeline in R
+##'
+# InputFiles is a list of .keg file names for parsing
 InputFiles<-read.table(file="Parse_Input_Files.txt",header=FALSE,sep = ".")
 for (i in 12:nrow(InputFiles)) {
   InputArg<-paste("python KEGG_parser.py", paste(InputFiles[i,1],InputFiles[i,2],sep = "."), "csv")
   system(InputArg)
   OutputArg<-paste(InputFiles[i,1],"xls",sep=".")
   nam<-paste("CompileHfile_",i,sep="")
-  # Read the files parsed by KEGG_parser into R
+  
+  # Read the parsed files into R in batches
   assign(nam, read.xls(OutputArg,sheet=1,header=TRUE))
 }
